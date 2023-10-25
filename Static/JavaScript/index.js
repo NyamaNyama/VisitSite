a1ab6a59986aa12d4ed36527dcb363b342f36932
Nyama N <>
2023-10-25 13:18:04 +1000

    Add gitignore and changing the button func

--- Static/JavaScript/index.js
+++ Static/JavaScript/index.js
@@ -1,13 +1,17 @@
 
-var click=true;
+
+var click=false;
 function clickText(){
     if(click){
         document.getElementById("Name").style.color='white';
-        document.getElementById("On").textContent="OFF"
+        document.getElementById("On").textContent="OFF";
+        overlay.style.backgroundImage= "url(backgrounds/pink.jpg";
+
     }
     else{
         document.getElementById("Name").style.color='black';
         document.getElementById("On").textContent="ON"
+        overlay.style.backgroundImage= "url(backgrounds/pink.jpg";
     }
-    click=!click;
+    click=!click;x
 }
