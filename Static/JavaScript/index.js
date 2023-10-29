97178e70eaca874984f032e874f99219328967f9
Nyama N <>
2023-10-29 12:20:30 +1000

    bug fixes

--- Static/JavaScript/index.js
+++ Static/JavaScript/index.js
@@ -1,17 +1,23 @@
-
-
 var click=false;
 function clickText(){
     if(click){
-        document.getElementById("Name").style.color='white';
+        var dark = document.getElementById("overlay");
+        document.getElementById("On").style.color='white';
+        document.getElementById("it").style.color='white';
         document.getElementById("On").textContent="OFF";
-        overlay.style.backgroundImage= "url(backgrounds/pink.jpg";
+        dark.style.backgroundColor="black";
+        dark.style.zIndex=0;
+        document.getElementById("switcher").style.textShadow="2px 2px 2px white";
 
     }
     else{
-        document.getElementById("Name").style.color='black';
+        var dark = document.getElementById("overlay");
+        document.getElementById("On").style.color='black';
+        document.getElementById("it").style.color='black';
         document.getElementById("On").textContent="ON"
-        overlay.style.backgroundImage= "url(backgrounds/pink.jpg";
+        dark.style.backgroundColor="transparent";
+        dark.style.zIndex=-1;
+        document.getElementById("switcher").style.textShadow="2px 2px 2px black";
     }
-    click=!click;x
+    click=!click;
 }
